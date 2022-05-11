483e3741e8e37d150b9cf8a5fb479bf9e111b3e8
anup <anup.kalbalia@gmail.com>
2022-05-12 03:27:28 +0530

    Added Day field to the detected_case table

--- app/Database/Migrations/2022-05-08-154356_DetectedCase.php
+++ app/Database/Migrations/2022-05-08-154356_DetectedCase.php
@@ -15,38 +15,32 @@ class DetectedCase extends Migration
                 'unsigned' => true,
                 'NOT NULL' => true,
                 'AUTO_INCREMENT' => true,
-
             ],
             'student_id' => [
                 'type' => 'BIGINT',
                 'unsigned' => true,
                 'NOT NULL' => true,
-
             ],
             'detection_criteria' => [
                 'type' => 'ENUM("7 Consecutive Days Absent ","<60% Attendance","0")',
                 'default' => '0',
-
             ],
             'status' => [
                 'type' => 'ENUM("Fresh","In Pursuit","Untraceable","Home Visit","Ticket-Raised-Normal-Resolved","Ticket-Raised-SOS","To Be Back To School","Not Sure","Refused","Back To School")',
                 'default' => 'Fresh',
-
             ],
             'date_of_bts' => [
-                'type' => 'varchar',
-                'constraint' => '10',
-
+                'type' => 'date',
+                'default' => null,
             ],
             'system_bts' => [
                 'type' => 'ENUM("Yes","No")',
                 'default' => 'No',
-
             ],
-
-
-
         ]);
+
+        $this->forge->addField(['`day` date default (curdate()) NOT NULL']);
+
 
         $this->forge->addPrimaryKey('case_id');
         $this->forge->createTable('detected_case');
