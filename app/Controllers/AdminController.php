62a34082eff28ef13cefc9a37abd2aefcef35172
anup <anup.kalbalia@gmail.com>
2022-05-16 03:37:10 +0530

    Added passing filter permission and on the user's authorisation credentials.

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -6,26 +6,34 @@ use App\Controllers\BaseController;
 
 class AdminController extends AuthController
 {
-    /*
-     * Permissions -
-        ['name' => 'manageUsers', 'description' => 'Create operators and other users and assign them groups'],
-        ['name' => 'viewAllReports', 'description' => 'View all reports'],
-        ['name' => 'viewReportsDistricts', 'description' => 'View reports of a district'],
-        ['name' => 'viewReportsZone', 'description' => 'View reports of a zone'],
-        ['name' => 'viewReportsSchool', 'description' => 'View reports of a school'],
-        ['name' => 'manageCases', 'description' => 'Access Operator functionality'],
-    */
+    /*    private array $permissions = [
+            ['name' => 'manageUsers', 'description' => 'Create operators and other users and assign them groups'],
+            ['name' => 'viewAllReports', 'description' => 'View all reports'],
+            ['name' => 'viewReportsDistricts', 'description' => 'View reports of a district'],
+            ['name' => 'viewReportsZone', 'description' => 'View reports of a zone'],
+            ['name' => 'viewReportsSchool', 'description' => 'View reports of a school'],
+            ['name' => 'manageCases', 'description' => 'Access Operator functionality']
+        ];*/
 
+    private static array $valid_permissions_for_dashboard = ['viewAllReports', 'viewReportsDistricts', 'viewReportsZone', 'viewReportsSchool'];
 
-    private function doesUserHavePermission($permissions)
+    private array $filters;
+
+    private function doesUserHavePermission(): bool
     {
         $userId = $this->authenticate->id();
-        return $this->authorize->doesUserHavePermission($userId, 'viewAllReports');
+        $result = true;
+        foreach ($this->valid_permissions_for_dashboard as $permission) {
+            $isPermitted = $this->authorize->doesUserHavePermission($userId, $permission);
+            $result &= $isPermitted;
+            $this->filters[] = [$permission => $isPermitted];
+        }
+        return $result;
     }
 
     public function index($report_type)
     {
-        if ($this->doesUserHavePermission(['viewAllReports', 'viewReportsDistricts', 'viewReportsZone', 'viewReportsSchool'])) {
+        if ($this->doesUserHavePermission()) {
             //TODO: The filters will change based on the permission that the user has.
 
             switch ($report_type) {
@@ -86,6 +94,7 @@ class AdminController extends AuthController
         $data['page_title'] = $page_title;
         $data['details'] = $details;
         $data['user_name'] = user()->username;
+        $data['filter_permissions'] = $this->filters;
         return view($view_name, $data);
     }
 }
