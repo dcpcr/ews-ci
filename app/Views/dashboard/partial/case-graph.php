b103bbf225f494d488d76db1218a55290090a4e2
nuron <49301108+praveen902012@users.noreply.github.com>
2022-05-17 15:52:37 +0530

    Modified the case graph page

--- app/Views/dashboard/partial/case-graph.php
+++ app/Views/dashboard/partial/case-graph.php
@@ -36,40 +36,40 @@ if(count($data)>0){
                                    data-height="100" data-fgColor="#efb155" data-readonly="true">'
 
 
-                            <div class="knob-label">Male---------<?=$male_count?> (<?= (count($data)>0)? "".floor($male_count/count($data)*100)."":"0";?>%)</div>
-                            <div class="knob-label">Female-------<?=$female_count?>(<?= (count($data)>0)? "".floor($female_count/count($data)*100)."":"0";?>%)</div>
-                            <div class="knob-label">Transgender--<?=$transgender_count?> (<?= (count($data)>0)? "".floor($transgender_count/count($data)*100)."":"0";?>%)</div>
+                            <div class="knob-label">Male-----------<?=$male_count?> (<?= (count($data)>0)? "".floor($male_count/count($data)*100)."":"0";?>%)</div>
+                            <div class="knob-label">Female--------<?=$female_count?>(<?= (count($data)>0)? "".floor($female_count/count($data)*100)."":"0";?>%)</div>
+                            <div class="knob-label">Transgender------<?=$transgender_count?> (<?= (count($data)>0)? "".floor($transgender_count/count($data)*100)."":"0";?>%)</div>
 
                         </div>
                         <div class="col-6 col-md-3 text-center">
                             <div class="knob-label"><strong>Suo Moto Cases</strong></div>
-                            <input disabled id="suomoto" type="text" class="knob" value="80" data-skin="tron"
+                            <input disabled id="suomoto" type="text" class="knob" value="0" data-skin="tron"
                                    data-thickness="0.2" data-width="100"
                                    data-height="100" data-fgColor="#cd4949" data-readonly="true">
 
-                            <div class="knob-label text-center">Male---------10(8%)</div>
-                            <div class="knob-label text-center">Female-------55(90%)</div>
-                            <div class="knob-label text-center">Transgender--5(2%)</div>
+                            <div class="knob-label text-center">Male---------0(0%)</div>
+                            <div class="knob-label text-center">Female-------0(0%)</div>
+                            <div class="knob-label text-center">Transgender--0(0%)</div>
                         </div>
                         <div class="col-6 col-md-3 text-center">
                             <div class="knob-label"><strong>Back to School</strong></div>
-                            <input disabled type="text" id="bts" class="knob" value="80" data-skin="tron"
+                            <input disabled type="text" id="bts" class="knob" value="0" data-skin="tron"
                                    data-thickness="0.2" data-width="100"
                                    data-height="100" data-fgColor="#28a745" data-readonly="true">
 
-                            <div class="knob-label">Male---------10(8%)</div>
-                            <div class="knob-label">Female-------55(90%)</div>
-                            <div class="knob-label">Transgender--5(2%)</div>
+                            <div class="knob-label">Male---------0(0%)</div>
+                            <div class="knob-label">Female-------0(0%)</div>
+                            <div class="knob-label">Transgender--0(0%)</div>
                         </div>
                         <div class="col-6 col-md-3 text-center">
                             <div class="knob-label"><strong>Pending / Untraceable Cases</strong></div>
-                            <input disabled type="text" id="pending" class="knob" value="80" data-skin="tron"
+                            <input disabled type="text" id="pending" class="knob" value="0" data-skin="tron"
                                    data-thickness="0.2" data-width="100"
                                    data-height="100" data-fgColor="#6d726e" data-readonly="true">
 
-                            <div class="knob-label">Male---------10(8%)</div>
-                            <div class="knob-label">Female-------55(90%)</div>
-                            <div class="knob-label">Transgender--5(2%)</div>
+                            <div class="knob-label">Male----------0(0%)</div>
+                            <div class="knob-label">Female--------0(0%)</div>
+                            <div class="knob-label">Transgender---0(0%)</div>
                         </div>
 
                     </div>
