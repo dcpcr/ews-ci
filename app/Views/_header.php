f4807c8e29437d7425cb6c23f9148735a835d0ed
nuron <49301108+praveen902012@users.noreply.github.com>
2022-06-08 20:08:14 +0530

    Fixed the login page layout

--- app/Views/_header.php
+++ app/Views/_header.php
@@ -6,21 +6,10 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
-
-    <title>Myth:Auth</title>
-
+    <title>Early Warning System</title>
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-
-    <style>
-        body {
-            padding-top: 5rem;
-        }
-    </style>
 </head>
-
 <body>
-
 <?= view('App\Views\_navbar') ?>
-
 <main role="main" class="container">
