59320b9f42bc1ee4328f28ab5a39ac0aa6e2bafa
praveen902012 <praveen.902012@gmail.com>
2022-06-14 11:58:43 +0530

    Added tooltips for case page

--- app/Views/dashboard/partial/case-graph.php
+++ app/Views/dashboard/partial/case-graph.php
@@ -1,15 +1,15 @@
 <?php
-$male_count=$female_count=$transgender_count=0;
-if(count($response)>0){
-    foreach ($response as $row){
+$male_count = $female_count = $transgender_count = 0;
+if (count($response) > 0) {
+    foreach ($response as $row) {
 
-        if($row['gender']=='Male'){
+        if ($row['gender'] == 'Male') {
             $male_count++;
         }
-        if($row['gender']=='Female'){
+        if ($row['gender'] == 'Female') {
             $female_count++;
         }
-        if($row['gender']=="Transgender"){
+        if ($row['gender'] == "Transgender") {
             $transgender_count++;
         }
     }
@@ -40,7 +40,11 @@ if(count($response)>0){
                             <div class="knob-label">Transgender------<?=$transgender_count?> (<?= (count($response)>0)? "".floor($transgender_count/count($response)*100)."":"0";?>%)</div>
                         </div>
                         <div class="col-6 col-md-2 text-center">
-                            <div class="knob-label pb-3"><strong>High Risk</strong></div>
+                            <div class="knob-label pb-3"><strong>High Risk</strong>
+                                <span class="fa fa-info-circle" data-toggle="tooltip" data-placement="right"
+                                      title="Students in need of urgent intervention, typically includes cases- Parental death, Parental Incarceration, Parent(s)' or Student's severe sickness or disability , Child labour, Child marriage, Children victim of sexual violence, Substance abuse, Children in conflict with law or any other such adversity for children not listed here.
+"></span>
+                            </div>
                             <input disabled id="highrisk" type="text" class="knob" value="0" data-skin="tron"
                                    data-thickness="0.2" data-width="100"
                                    data-height="100" data-fgColor="#cd4949" data-readonly="true">
@@ -50,7 +54,10 @@ if(count($response)>0){
                             <div class="knob-label text-center">Transgender--0(0%)</div>
                         </div>
                         <div class="col-6 col-md-2 text-center">
-                            <div class="knob-label pb-3"><strong>Back to School</strong></div>
+                            <div class="knob-label pb-3"><strong>Back to School</strong> <span class="fa fa-info-circle"
+                                                                                               data-toggle="tooltip"
+                                                                                               data-placement="right"
+                                                                                               title="When a high risk student re-joins the school (attends the school at least a day from the long absence)."></span></div>
                             <input disabled type="text" id="bts" class="knob" value="0" data-skin="tron"
                                    data-thickness="0.2" data-width="100"
                                    data-height="100" data-fgColor="#28a745" data-readonly="true">
@@ -60,7 +67,11 @@ if(count($response)>0){
                             <div class="knob-label">Transgender--0(0%)</div>
                         </div>
                         <div class="col-6 col-md-2 text-center ">
-                            <div class="knob-label pb-3"><strong>Untraceable</strong></div>
+                            <div class="knob-label pb-3"><strong>Untraceable</strong> <span class="fa fa-info-circle"
+                                                                                            data-toggle="tooltip"
+                                                                                            data-placement="right"
+                                                                                            title="Cases wherein no contact could be established throughout (phone calls and home-visit)."></span>
+                            </div>
                             <input disabled type="text" id="untraceable" class="knob" value="0" data-skin="tron"
                                    data-thickness="0.2" data-width="100"
                                    data-height="100" data-fgColor="#6d726e" data-readonly="true">
@@ -70,7 +81,9 @@ if(count($response)>0){
                             <div class="knob-label">Transgender---0(0%)</div>
                         </div>
                         <div class="col-6 col-md-2 text-center ">
-                            <div class="knob-label pb-3"><strong>Yet to be contacted</strong></div>
+                            <div class="knob-label pb-3"><strong>Yet to be contacted</strong> <span
+                                        class="fa fa-info-circle" data-toggle="tooltip" data-placement="right"
+                                        title="Fresh cases or the cases wherein the first call is yet to be attempted."></span></div>
                             <input disabled type="text" id="fresh" class="knob" value="0" data-skin="tron"
                                    data-thickness="0.2" data-width="100"
                                    data-height="100" data-fgColor="#6d726e" data-readonly="true">
@@ -81,14 +94,6 @@ if(count($response)>0){
                         </div>
 
                     </div>
-                    <!-- ./col -->
                 </div>
             </div>
-        </div><!-- /.card-body -->
-    </div>
-    <!-- ./card -->
-</div>
-<!-- /.col -->
-</div>
-<!-- /.row -->
-		
+        </div>
