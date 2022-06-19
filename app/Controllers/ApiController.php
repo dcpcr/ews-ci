085a62d0d1b64b4660099bd72f0906e7690c100e
nuron <49301108+praveen902012@users.noreply.github.com>
2022-06-19 09:58:43 +0530

    extended the resourceController and added error messages in json fromat

--- app/Controllers/ApiController.php
+++ app/Controllers/ApiController.php
@@ -2,32 +2,34 @@
 
 namespace App\Controllers;
 
-use App\Controllers\BaseController;
 use App\Models\CaseModel;
-
-class ApiController extends BaseController
+use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\API\ResponseTrait;
+class ApiController extends ResourceController
 {
-    public function index()
+    use ResponseTrait;
+    public function apiDetectedCase()
     {
         //TODO: Add Authentication for API request
-        // date fromat yyyy-mm-dd and url for api request => http://localhost:8080/APIcontroller/?fromdate=2022-01-01&todate=2022-05-25&limit=0&pageno=1
+        // date fromat yyyy-mm-dd
+        // test url for api request => http://localhost:8080/APIcontroller/apiDetectedCase/?fromdate=2022-01-01&todate=2022-05-25&limit=1&pageno=1
         //header('Content-Type:application/json');
         if ( $_GET['fromdate'] and $_GET['todate'] !== null) {
 
-            preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$_GET['fromdate'])? :die("Invalid date format for request parameter fromdate.");
-            preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$_GET['todate'])? :die("Invalid date format for request parameter todate.");
+            preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$_GET['fromdate'])? :die(json_encode(["message"=>"Invalid date format for request parameter fromdate."]));
+            preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$_GET['todate'])? :die(json_encode(["message"=>"Invalid date format for request parameter todate."]));
 
             if(!isset($_GET['limit'])){
                 $no_of_records_per_page=1000;
             }
             else{
-                $_GET['limit']<=0? die("Invalid Limit"):$no_of_records_per_page=$_GET['limit'];
+                $_GET['limit']<=0? die(json_encode(["message"=>"Invalid limit"])):$no_of_records_per_page=$_GET['limit'];
             }
             if(!isset($_GET['pageno'])){
                 $pageno=1;
 
             }else{
-                $_GET['pageno']<=0? die("Invalid Page Number"):$pageno=$_GET['pageno'];
+                $_GET['pageno']<=0? die(json_encode(["message"=>"Invalid page number"])):$pageno=$_GET['pageno'];
             }
             $case_model = new CaseModel();
             $total_rows=$case_model->getTotalNumberofCaseData($_GET['fromdate'],$_GET['todate']);
