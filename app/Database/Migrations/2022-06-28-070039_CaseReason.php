9faf0538d971bfa87f7ab422e062ad82c4e161d8
praveen902012 <praveen.902012@gmail.com>
2022-06-28 14:48:35 +0530

    Added Migration for case_reason table to store the reason of the cases

---
+++ app/Database/Migrations/2022-06-28-070039_CaseReason.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CaseReason extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'case_id' => [
+                'type' => 'BIGINT',
+                'constraint' => 10,
+                'unsigned' => true,
+            ],
+            'reason_id' => [
+                'type' => 'INT',
+                'constraint' => '3',
+                'null' => false
+            ],
+
+            'updated_at' => [
+                'type' => 'datetime',
+                'null' => true,
+            ],
+            'created_at datetime default current_timestamp',
+        ]);
+        $this->forge->createTable('case_reason');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('case_reason');
+    }
+}
