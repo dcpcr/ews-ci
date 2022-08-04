640eea38a6ca24e626599a07aa7eabf8c55ec0f6
Praveen <praveen.902012@gmail.com>
2022-08-04 14:52:34 +0530

    Cron Controller :A function and cron has been added to send the promotional messages to all the students. Routes : A new entry has been added to send the promotional sms to all the students.

--- app/Models/StudentModel.php
+++ app/Models/StudentModel.php
@@ -59,12 +59,17 @@ class StudentModel extends Model
         }
     }
 
-    public function getNewStudentMobileNumbers(): array
+    public function getNewStudentMobileNumbers($limit = '', $offset = ''): array
     {
 
         $previous_date = date("Y-m-d", strtotime('-1 day', strtotime(date("Y-m-d"))));
-        return $this->select(['mobile'])->where("date_format(`created_at`,'%Y-%m-%d') =", $previous_date)->findAll();
+        return $this->select(['mobile'])->where("date_format(`created_at`,'%Y-%m-%d') =", $previous_date)->findAll("$limit", "$offset");
+    }
 
+    public function getTotalStudentCount()
+    {
+        $count = $this->selectCount('id')->findAll();
+        return $count[0]['id'];
     }
 
 }
