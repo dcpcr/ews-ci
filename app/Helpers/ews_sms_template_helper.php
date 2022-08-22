5e336a8148c206a44f56b7e9630310899968b1fc
nuron <49301108+praveen902012@users.noreply.github.com>
2022-08-22 11:21:24 +0530

    added helpline sms template helper

--- app/Helpers/ews_sms_template_helper.php
+++ app/Helpers/ews_sms_template_helper.php
@@ -60,14 +60,3 @@ function case_closed_sms($mobile_number, $student_id, $student_name)
     $template_id = "1307165967783295949";
     return send_single_unicode_sms($message_unicode, $mobile_number, $template_id,);
 }
-
-/**
- * @throws ReflectionException
- */
-function bulk_helpline_promotion_sms($mobile_numbers)
-{
-    //$mobile_numbers is an array
-    $message_unicode = "बच्चे को सेहत, पोषण, या कोई और समस्या हो या संबंधित जानकारी चाहिए, तो DCPCR दिल्ली सरकार हेल्पलाइन 9311551393 पर कॉल करें।DCPCR";
-    $template_id = "1307162126864296262";
-    return send_bulk_unicode_sms($message_unicode, $mobile_numbers, $template_id);
-}
