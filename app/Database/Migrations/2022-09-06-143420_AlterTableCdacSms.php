bedf5504906f0660d1670f5cc778d222d75ede54
nuron <praveen.902012@gmail.com>
2022-09-06 21:08:25 +0530

    Added new column "verify" in cdac_sms table

---
+++ app/Database/Migrations/2022-09-06-143420_AlterTableCdacSms.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AlterTableCdacSms extends Migration
+{
+    protected $DBGroup = 'master';
+    public function up()
+    {
+        $fields = [
+            'verify' => ['type' => 'INT', 'constraint' => 1, 'after' => 'report_fetched'],
+        ];
+        $this->forge->addColumn('cdac_sms', $fields);
+
+
+    }
+
+    public function down()
+    {
+        //
+    }
+}
