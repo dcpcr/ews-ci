90d12cda1ec57b44108df14260ca3d88b1da957d
nuron <praveen.902012@gmail.com>
2022-09-22 17:46:11 +0530

    fetch data for reason_for_absenteeism module done

--- app/Helpers/cyfuture_helper.php
+++ app/Helpers/cyfuture_helper.php
@@ -1,4 +1,7 @@
 <?php
+
+use App\Models\ReasonForAbsenteeismModel;
+
 function get_cyfuture_ewsrecord_url()
 {
     return getenv('cyfuture_ewsrecord_url');
@@ -40,32 +43,49 @@ function get_cyfuture_token()
         log_message("error", "The API call failed, url - " . $url);
 
     }
+    return [];
 }
 
+/**
+ * @throws ReflectionException
+ */
 function download_operator_form_data()
 {
-
     $token = get_cyfuture_Token();
-    $url = get_cyfuture_ewsrecord_url();
-    $username = get_cyfuture_username();
-    $password = get_cyfuture_password();
-    $method = get_cyfuture_method();
-    $from_date = '2022-07-01';
-    $to_date = date("Y-m-d");
-    $response = get_curl_response($url, $username, $password, $method, $from_date, $to_date, $token);
-    if ($response) {
-        log_message("info", "The Cyfuture EWS record API call success, url - " . $url);
-        $decoded_json = json_decode($response, true);
-        return $decoded_json['data'];
+    if (!empty($token)) {
+        $url = get_cyfuture_ewsrecord_url();
+        $username = get_cyfuture_username();
+        $password = get_cyfuture_password();
+        $method = get_cyfuture_method();
+        $from_date = '2022-07-01';
+        $to_date = date("Y-m-d");
+        $page_number = 1;
+        $record_count = 0;
+        do {
+            $response = get_curl_response($url, $username, $password, $method, $from_date, $to_date, $page_number, $token);
+            $decoded_json = json_decode($response, true);
+            $total_pages = $decoded_json['total_pages'];
+            if ($response) {
+                $cases = $decoded_json['data'];
+                $record_count = $record_count + count($cases);
+                $reason_for_absenteeism_model = new ReasonForAbsenteeismModel();
+                $reason_for_absenteeism_model->insertUpdateCaseReason($cases);
+                log_message("info", "The Cyfuture EWS record API call success, for Page - " . $page_number);
+            } else {
+                log_message("error", "The Cyfuture EWS record API call failed, Page -" . $page_number . "url - " . $url);
+            }
+            $page_number++;
+        } while ($page_number <= $total_pages);
+        log_message("info", "Total Records fetched from Cyfuture EWS record API, ->" . $record_count);
     } else {
-        log_message("error", "The Cyfuture EWS record API call failed, url - " . $url);
-
+        log_message("error", "Cyfuture EWS token API failed.");
     }
 
 }
+
 function extract_reason_data_from_cases($cases): array
 {
-    $keys = ['case_id', 'reason_of_absense','other_reason_of_absense'];
+    $keys = ['case_id', 'reason_of_absense', 'other_reason_of_absense'];
     return extract_values_from_objects($cases, $keys);
 }
 
@@ -74,6 +94,7 @@ function extract_call_disposition_data_from_cases($cases): array
     $keys = ['case_id', 'call_dis'];
     return extract_values_from_objects($cases, $keys);
 }
+
 //raised_ticket
 function extract_high_risk_data_from_cases($cases): array
 {
@@ -95,7 +116,7 @@ function extract_home_visit_data_from_cases($cases): array
 
 function extract_dcpcr_helpline_ticket_data_from_cases($cases): array
 {
-    $keys = ['case_id', 'name_division','sub_name_division','nature_case',];
+    $keys = ['case_id', 'name_division', 'sub_name_division', 'nature_case',];
     return extract_values_from_objects($cases, $keys);
 }
 
