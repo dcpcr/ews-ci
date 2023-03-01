32d34fec0e7cc27df847a45bfe880a2fa16fb9ab
praveen902012 <praveen.902012@gmail.com>
2023-03-01 13:24:11 +0530

    removed key from json data of credential encryption

--- app/ThirdParty/myth-auth/src/Controllers/AuthController.php
+++ app/ThirdParty/myth-auth/src/Controllers/AuthController.php
@@ -471,10 +471,10 @@ class AuthController extends Controller
     {
         $jsondata = json_decode($jsonString, true);
         $salt = hex2bin($jsondata["s"]);
-        $ct = base64_decode($jsondata["ct"]);
         $iv = hex2bin($jsondata["iv"]);
+        $ct = base64_decode($jsondata["ct"]);
         $concatedPassphrase = $passphrase . $salt;
-        $md5 = array();
+        $md5 = [];
         $md5[0] = md5($concatedPassphrase, true);
         $result = $md5[0];
         for ($i = 1; $i < 3; $i++) {
