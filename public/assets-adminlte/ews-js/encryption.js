9595286b5a0df1f78d56d919980323654307941c
praveen902012 <praveen.902012@gmail.com>
2023-03-01 13:24:11 +0530

    removed key from json data of credential encryption

--- public/assets-adminlte/ews-js/encryption.js
+++ public/assets-adminlte/ews-js/encryption.js
@@ -1,23 +1,34 @@
-const CryptoJSAesJson = {
-    stringify: function (cipherParams) {
-        const j = {ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64)};
-        if (cipherParams.iv) j.iv = cipherParams.iv.toString();
-        if (cipherParams.salt) j.s = cipherParams.salt.toString();
-        return JSON.stringify(j);
+var CryptoJSAesJson = {
+    /**
+     * Encrypt any value
+     * @param {*} value
+     * @param {string} password
+     * @return {string}
+     */
+    'encrypt': function (value, password) {
+        return CryptoJS.AES.encrypt(JSON.stringify(value), password, { format: CryptoJSAesJson }).toString()
     },
-    parse: function (jsonStr) {
-        const j = JSON.parse(jsonStr);
-        const cipherParams = CryptoJS.lib.CipherParams.create({ciphertext: CryptoJS.enc.Base64.parse(j.ct)});
-        if (j.iv) cipherParams.iv = CryptoJS.enc.Hex.parse(j.iv)
-        if (j.s) cipherParams.salt = CryptoJS.enc.Hex.parse(j.s)
-        return cipherParams;
-    }
+    /**
+     * Stringify cryptojs data
+     * @param {Object} cipherParams
+     * @return {string}
+     */
+    'stringify': function (cipherParams) {
+        var j = { ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64) }
+        if (cipherParams.iv) j.iv = cipherParams.iv.toString()
+        if (cipherParams.salt) j.s = cipherParams.salt.toString()
+        return JSON.stringify(j).replace(/\s/g, '')
+    },
 }
-
 function check() {
     const password = document.getElementById("password").value;
     if (password !== null) {
-        document.getElementById("password").value = CryptoJS.AES.encrypt(JSON.stringify(password), key, {format: CryptoJSAesJson}).toString();
+        var encrypted = CryptoJSAesJson.encrypt(
+            document.getElementById("password").value,
+            key
+        );
+        alert(encrypted);
+        document.getElementById("password").value = encrypted;
         return true;
     } else {
         return false;
