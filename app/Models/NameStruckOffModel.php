e9f846ef576395f0356d0544e0dbfa1d31b3030e
praveen902012 <praveen.902012@gmail.com>
2023-03-16 12:15:06 +0530

    added slc and nso model and fetch data function in edutel helper file

---
+++ app/Models/NameStruckOffModel.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class NameStruckOffModel extends Model
+{
+    protected $DBGroup = 'default';
+    protected $table = 'name_struck_off';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID = 0;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
+    protected $protectFields = true;
+    protected $allowedFields = ['school_id', 'school_name', 'student_id', 'student_name', 'nso_date', 'reason'];
+
+    /**
+     * @throws \ReflectionException
+     */
+    public function fetchNameStruckOffData()
+    {
+
+        helper('edutel');
+        $data = fetch_name_struck_out_data_from_edudel();
+        $table_data = [];
+        foreach ($data as $row) {
+            $table_data[] = [
+                "school_id" => $row['schid'],
+                "school_name" => $row['schname'],
+                "student_id" => $row['StudentID'],
+                "student_name" => $row['name'],
+                "nso_date" => $row['NSODate'],
+                "reason" => $row['Reason']
+            ];
+        }
+
+        $count = $this->ignore()->insertBatch($table_data, null, count($table_data));
+        log_message("info", "$count new records inserted in school_leaving_certificate table.");
+        $count = $this->updateBatch($table_data, 'student_id', 2000);
+        log_message("info", "$count records updated in school_leaving_certificate table.");
+
+    }
+
+
+}
